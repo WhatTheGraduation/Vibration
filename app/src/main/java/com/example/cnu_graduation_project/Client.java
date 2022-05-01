e02adae0bffafee4945f200f38d47668f5db1d75
jaeyong <jaeyong@jaeyongui-MacBookAir.local>
2022-05-01 21:47:12 +0900

    API 받아오기

--- app/src/main/java/com/example/cnu_graduation_project/Client.java
+++ app/src/main/java/com/example/cnu_graduation_project/Client.java
@@ -1,84 +1,259 @@
 package com.example.cnu_graduation_project;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import static android.app.PendingIntent.*;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import com.example.cnu_graduation_project.LogFragment;
 import com.google.android.gms.location.ActivityRecognition;
-import com.google.android.gms.location.ActivityRecognitionClient;
 import com.google.android.gms.location.ActivityTransition;
+import com.google.android.gms.location.ActivityTransitionEvent;
 import com.google.android.gms.location.ActivityTransitionRequest;
+import com.google.android.gms.location.ActivityTransitionResult;
 import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
- *
- * 사용자에게 보여지는 첫 페이지 / 실행 관리 페이지
- *
+ * Demos enabling/disabling Activity Recognition transitions, e.g., starting or stopping a walk,
+ * run, drive, etc.).
  */
-
 public class Client extends AppCompatActivity {
 
-    ActivityRecognitionClient client;
-    //시작시 보여주는 text
-    TextView drivingModeView;
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    @SuppressLint("SetTextI18n")
+    private final static String TAG = "MainActivity";
+
+    // TODO: Review check for devices with Android 10 (29+).
+    private boolean runningQOrLater =
+            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
+
+    private boolean activityTrackingEnabled;
+
+    private List<ActivityTransition> activityTransitionList;
+
+    // Action fired when transitions are triggered.
+    private final String TRANSITIONS_RECEIVER_ACTION =
+            BuildConfig.APPLICATION_ID + "TRANSITIONS_RECEIVER_ACTION";
+
+    private PendingIntent mActivityTransitionsPendingIntent;
+    private TransitionsReceiver mTransitionsReceiver;
+    private LogFragment mLogFragment;
+
+    private static String toActivityString(int activity) {
+        switch (activity) {
+            case DetectedActivity.STILL:
+                return "STILL";
+            case DetectedActivity.WALKING:
+                return "WALKING";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    private static String toTransitionType(int transitionType) {
+        switch (transitionType) {
+            case ActivityTransition.ACTIVITY_TRANSITION_ENTER:
+                return "ENTER";
+            case ActivityTransition.ACTIVITY_TRANSITION_EXIT:
+                return "EXIT";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.client);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        mLogFragment =
+                (LogFragment) getSupportFragmentManager().findFragmentById(R.id.log_fragment);
 
-        drivingModeView = findViewById(R.id.driving_mode);
-        /**
-         * API 받아오는 시작부분
-         */
-        if (activityRecognitionPermissionApproved(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q)) {
-            drivingModeView.setText("API를 받아올 수 없습니다.");
-        } else {
-            drivingModeView.setText("운전 중이 아닙니다.");
+        activityTrackingEnabled = false;
+
+        // List of activity transitions to track.
+        activityTransitionList = new ArrayList<>();
+
+        // TODO: Add activity transitions to track.
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                .build());
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                .build());
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.STILL)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                .build());
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.STILL)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                .build());
+
+        // TODO: Initialize PendingIntent that will be triggered when a activity transition occurs.
+        Intent intent = new Intent(TRANSITIONS_RECEIVER_ACTION);
+        mActivityTransitionsPendingIntent =
+                PendingIntent.getBroadcast(Client.this, 0, intent, FLAG_MUTABLE);
+
+        // TODO: Create a BroadcastReceiver to listen for activity transitions.
+        // The receiver listens for the PendingIntent above that is triggered by the system when an
+        // activity transition occurs.
+        mTransitionsReceiver = new TransitionsReceiver();
+
+        printToScreen("App initialized.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: Register the BroadcastReceiver to listen for activity transitions.
+        registerReceiver(mTransitionsReceiver, new IntentFilter(TRANSITIONS_RECEIVER_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+
+        // TODO: Disable activity transitions when user leaves the app.
+        if (activityTrackingEnabled) {
+            disableActivityTransitions();
         }
-        client = ActivityRecognition.getClient(this);
-        requestforUpdate();
+        super.onPause();
     }
 
-    private void requestforUpdate() {
-        client.requestActivityTransitionUpdates(
-                getTrangitionRequest(),
-                getPendingIntent()
-        );
-    }
-    private PendingIntent getPendingIntent(){
-        Intent intent = new Intent(this, DrivingRecognition.class);
 
-        return PendingIntent.getBroadcast(this,
-                0,
-                intent,
-                PendingIntent.FLAG_IMMUTABLE);
+    @Override
+    protected void onStop() {
+
+        // TODO: Unregister activity transition receiver when user leaves the app.
+        unregisterReceiver(mTransitionsReceiver);
+
+        super.onStop();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Start activity recognition if the permission was approved.
+        if (activityRecognitionPermissionApproved() && !activityTrackingEnabled) {
+            enableActivityTransitions();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     /**
-     * @author 재용
-     * api 사용 권환 획득을 위한 함수
-     * api를 받아오지 못하면 permission 요청
-     * @param runningQOrLater
-     *
-     * @return
-     * 성공시 ture 실패시 false
+     * Registers callbacks for {@link ActivityTransition} events via a custom
+     * {@link BroadcastReceiver}
      */
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    private boolean activityRecognitionPermissionApproved(boolean runningQOrLater){
-    // TODO: Review permission check for 29+.
+    private void enableActivityTransitions() {
+
+        Log.d(TAG, "enableActivityTransitions()");
+
+
+        // TODO: Create request and listen for activity changes.
+        ActivityTransitionRequest request = new ActivityTransitionRequest(activityTransitionList);
+
+        // Register for Transitions Updates.
+        Task<Void> task = ActivityRecognition.getClient(this)
+                .requestActivityTransitionUpdates(request, mActivityTransitionsPendingIntent);
+
+        task.addOnSuccessListener(
+                new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        activityTrackingEnabled = true;
+                        printToScreen("Transitions Api was successfully registered.");
+
+                    }
+                });
+
+        task.addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        printToScreen("Transitions Api could NOT be registered: " + e);
+                        Log.e(TAG, "Transitions Api could NOT be registered: " + e);
+
+                    }
+                });
+    }
+
+
+    /**
+     * Unregisters callbacks for {@link ActivityTransition} events via a custom
+     * {@link BroadcastReceiver}
+     */
+    private void disableActivityTransitions() {
+
+        Log.d(TAG, "disableActivityTransitions()");
+
+
+        // TODO: Stop listening for activity changes.
+        ActivityRecognition.getClient(this).removeActivityTransitionUpdates(mActivityTransitionsPendingIntent)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        activityTrackingEnabled = false;
+                        printToScreen("Transitions successfully unregistered.");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        printToScreen("Transitions could not be unregistered: " + e);
+                        Log.e(TAG,"Transitions could not be unregistered: " + e);
+                    }
+                });
+    }
+
+    /**
+     * On devices Android 10 and beyond (29+), you need to ask for the ACTIVITY_RECOGNITION via the
+     * run-time permissions.
+     */
+    private boolean activityRecognitionPermissionApproved() {
+
+        // TODO: Review permission check for 29+.
         if (runningQOrLater) {
 
             return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
@@ -90,28 +265,62 @@ public class Client extends AppCompatActivity {
         }
     }
 
+    public void onClickEnableOrDisableActivityRecognition(View view) {
 
-    public ActivityTransitionRequest getTrangitionRequest(){
-        List<ActivityTransition> transitions = new ArrayList<>();
+        // TODO: Enable/Disable activity tracking and ask for permissions if needed.
+        if (activityRecognitionPermissionApproved()) {
 
-        transitions.add(
-                new ActivityTransition.Builder()
-                        .setActivityType(DetectedActivity.IN_VEHICLE)
-                        .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-                        .build());
+            if (activityTrackingEnabled) {
+                disableActivityTransitions();
 
-        transitions.add(
-                new ActivityTransition.Builder()
-                        .setActivityType(DetectedActivity.IN_VEHICLE)
-                        .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                        .build());
+            } else {
+                enableActivityTransitions();
+            }
 
-        transitions.add(
-                new ActivityTransition.Builder()
-                        .setActivityType(DetectedActivity.STILL)
-                        .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                        .build());
+        } else {
+            // Request permission and start activity for result. If the permission is approved, we
+            // want to make sure we start activity recognition tracking.
+            Intent startIntent = new Intent(this, PermissionRationalActivity.class);
+            startActivityForResult(startIntent, 0);
+        }
+    }
 
-        return new ActivityTransitionRequest(transitions);
+    private void printToScreen(@NonNull String message) {
+        mLogFragment.getLogView().println(message);
+        Log.d(TAG, message);
+    }
+
+    /**
+     * Handles intents from from the Transitions API.
+     */
+    public class TransitionsReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Log.d(TAG, "onReceive(): " + intent);
+
+            if (!TextUtils.equals(TRANSITIONS_RECEIVER_ACTION, intent.getAction())) {
+
+                printToScreen("Received an unsupported action in TransitionsReceiver: action = " +
+                        intent.getAction());
+                return;
+            }
+
+            // TODO: Extract activity transition information from listener.
+            if (ActivityTransitionResult.hasResult(intent)) {
+
+                ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
+
+                for (ActivityTransitionEvent event : result.getTransitionEvents()) {
+
+                    String info = "Transition: " + toActivityString(event.getActivityType()) +
+                            " (" + toTransitionType(event.getTransitionType()) + ")" + "   " +
+                            new SimpleDateFormat("HH:mm:ss", Locale.US).format(new Date());
+
+                    printToScreen(info);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/cnu_graduation_project/DrivingRecognition.java
+++ app/src/main/java/com/example/cnu_graduation_project/DrivingRecognition.java
@@ -4,37 +4,67 @@ import android.app.IntentService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.ActivityTransition;
 import com.google.android.gms.location.ActivityTransitionEvent;
 import com.google.android.gms.location.ActivityTransitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
-public class DrivingRecognition extends BroadcastReceiver {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DrivingRecognition extends BroadcastReceiver{
+
+    private static final String TAG = "RECEIVE";
+    private void printToScreen(@NonNull String message) {
+        Log.d(TAG, message);
+    }
+    private static String toActivityString(int activity) {
+        switch (activity) {
+            case DetectedActivity.STILL:
+                return "STILL";
+            case DetectedActivity.WALKING:
+                return "WALKING";
+            default:
+                return "UNKNOWN";
+        }
+    }
+    private static String toTransitionType(int transitionType) {
+        switch (transitionType) {
+            case ActivityTransition.ACTIVITY_TRANSITION_ENTER:
+                return "ENTER";
+            case ActivityTransition.ACTIVITY_TRANSITION_EXIT:
+                return "EXIT";
+            default:
+                return "UNKNOWN";
+        }
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (ActivityTransitionResult.hasResult(intent)) {
-            ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
-            for (ActivityTransitionEvent event : result.getTransitionEvents()) {
-                // chronological sequence of events....
-                switch (event.getActivityType()){
-                    case DetectedActivity
-                            .IN_VEHICLE:
-                        Log.d("test","탈것");
-                        break;
+        Log.d(TAG, "onReceive(): " + intent);
 
-                    case DetectedActivity
-                            .STILL:
-                        Log.d("test","still");
-                        break;
-                    case DetectedActivity
-                            .WALKING:
-                        Log.d("test","걷기");
-                        break;
-                }
+
+        // TODO: Extract activity transition information from listener.
+        if (ActivityTransitionResult.hasResult(intent)) {
+
+            ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
+
+            for (ActivityTransitionEvent event : result.getTransitionEvents()) {
+
+                String info = "Transition: " + toActivityString(event.getActivityType()) +
+                        " (" + toTransitionType(event.getTransitionType()) + ")" + "   " +
+                        new SimpleDateFormat("HH:mm:ss", Locale.US).format(new Date());
+                Toast.makeText(context.getApplicationContext(), "뭔가 일어남",Toast.LENGTH_LONG).show();
+                printToScreen(info);
             }
         }
 
